12ab967da6642d05013080e1dffbef9abf1bedeb
albin29 <albin.duraku00@gmail.com>
2024-01-21 01:16:17 +0100

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Client
+{
+    static async Task Main()
+    {
+        IPAddress ipAddress = IPAddress.Parse("ipDuSkaConnecta");
+        int port = 27015;
+
+        TcpClient client = new TcpClient();
+        await client.ConnectAsync(ipAddress, port);
+
+        try
+        {
+            using NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                Console.Write("Enter a message to send to the server (or type 'exit' to quit): ");
+                string message = Console.ReadLine();
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                await stream.WriteAsync(buffer, 0, buffer.Length);
+
+                Console.WriteLine("Message sent to server");
+            }
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Server
+{
+    static async Task Main()
+    {
+        IPAddress ipAddress = IPAddress.Parse("0.0.0.0");
+        int port = 27015;
+
+        TcpListener listener = new TcpListener(ipAddress, port);
+
+        try
+        {
+            listener.Start();
+            Console.WriteLine($"Server listening on {ipAddress}:{port}");
+
+            TcpClient client = await listener.AcceptTcpClientAsync();
+            Console.WriteLine($"Accepted connection from {((IPEndPoint)client.Client.RemoteEndPoint).Address}");
+
+            using NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                byte[] buffer = new byte[1];
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Received message: {receivedMessage}");
+            }
+        }
+        finally
+        {
+            listener.Stop();
+        }
+    }
+}
